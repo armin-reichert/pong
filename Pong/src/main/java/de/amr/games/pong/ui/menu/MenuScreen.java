87ad51477555a833db4cab6a9d23634b6ba95080
Armin Reichert <armin.reichert@web.de>
2020-02-15 13:05:55 +0100

    refactoring

--- Pong/src/main/java/de/amr/games/pong/ui/menu/MenuScreen.java
+++ Pong/src/main/java/de/amr/games/pong/ui/menu/MenuScreen.java
@@ -1,12 +1,10 @@
 package de.amr.games.pong.ui.menu;
 
 import static de.amr.easy.game.Application.LOGGER;
-import static de.amr.easy.game.input.Keyboard.keyPressedOnce;
 import static de.amr.games.pong.model.Game.PlayMode.Computer_Computer;
 import static de.amr.games.pong.model.Game.PlayMode.Computer_Player2;
 import static de.amr.games.pong.model.Game.PlayMode.Player1_Computer;
 import static de.amr.games.pong.model.Game.PlayMode.Player1_Player2;
-import static java.awt.event.KeyEvent.VK_ENTER;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -16,8 +14,8 @@ import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 
 import de.amr.easy.game.controller.Lifecycle;
-import de.amr.easy.game.entity.Entity;
 import de.amr.easy.game.input.Keyboard;
+import de.amr.easy.game.view.View;
 import de.amr.games.pong.model.Game.PlayMode;
 import de.amr.games.pong.ui.ScreenManager;
 import de.amr.statemachine.core.StateMachine;
@@ -27,66 +25,63 @@ import de.amr.statemachine.core.StateMachine;
  * 
  * @author Armin Reichert
  */
-public class MenuScreen extends Entity implements Lifecycle {
+public class MenuScreen extends StateMachine<PlayMode, Void> implements View, Lifecycle {
 
 	private final ScreenManager screenManager;
 	private final Dimension size;
-	private final StateMachine<PlayMode, Object> fsm;
 	private Color bgColor;
 	private Color bgColorSelected;
 	private Color hilightColor;
 
 	public MenuScreen(ScreenManager screenManager, Dimension size) {
+		super(PlayMode.class);
 		this.screenManager = screenManager;
 		this.size = size;
-		fsm = createStateMachine();
-		fsm.getTracer().setLogger(LOGGER);
-	}
-
-	private StateMachine<PlayMode, Object> createStateMachine() {
-		return
+		beginStateMachine()
 		//@formatter:off
-		StateMachine.beginStateMachine(PlayMode.class, Object.class)
 			.description("Pong Menu")
 			.initialState(Player1_Player2)
 			
 			.states()
-			
-				.state(Player1_Player2)
 
+				// for clarity, all states are listed, would also work without!
+				.state(Player1_Player2)
 				.state(Player1_Computer)
-				
 				.state(Computer_Player2)
-				
 				.state(Computer_Computer)
 		
 			.transitions()
-				.when(PlayMode.Player1_Player2).then(Player1_Computer)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_DOWN))
-	
-				.when(PlayMode.Player1_Computer).then(Computer_Player2)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_DOWN))
+			
+				.when(Player1_Player2)	.then(Player1_Computer)	.condition(this::nextEntrySelected)
+				.when(Player1_Computer)	.then(Computer_Player2)	.condition(this::nextEntrySelected)
+				.when(Computer_Player2)	.then(Computer_Computer).condition(this::nextEntrySelected)
+				.when(Computer_Computer).then(Player1_Player2)	.condition(this::nextEntrySelected)
 				
-				.when(PlayMode.Computer_Player2).then(Computer_Computer)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_DOWN))
-				
-				.when(PlayMode.Computer_Computer).then(Player1_Player2)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_DOWN))
-				
-				.when(PlayMode.Player1_Player2).then(Computer_Computer)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_UP))
-				
-				.when(PlayMode.Computer_Computer).then(Computer_Player2)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_UP))
-				
-				.when(PlayMode.Computer_Player2).then(Player1_Computer)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_UP))
-				
-				.when(PlayMode.Player1_Computer).then(Player1_Player2)
-					.condition(() -> Keyboard.keyPressedOnce(KeyEvent.VK_UP))
+				.when(Player1_Player2)	.then(Computer_Computer).condition(this::prevEntrySelected)
+				.when(Computer_Computer).then(Computer_Player2)	.condition(this::prevEntrySelected)
+				.when(Computer_Player2)	.then(Player1_Computer)	.condition(this::prevEntrySelected)
+				.when(Player1_Computer)	.then(Player1_Player2)	.condition(this::prevEntrySelected)
 		
 		.endStateMachine();
 		//@formatter:on
+		getTracer().setLogger(LOGGER);
+	}
+
+	private boolean nextEntrySelected() {
+		return Keyboard.keyPressedOnce(KeyEvent.VK_DOWN);
+	}
+
+	private boolean prevEntrySelected() {
+		return Keyboard.keyPressedOnce(KeyEvent.VK_UP);
+	}
+
+	@Override
+	public void setVisible(boolean visible) {
+	}
+
+	@Override
+	public boolean visible() {
+		return true;
 	}
 
 	public void setBgColor(Color bgColor) {
@@ -102,16 +97,11 @@ public class MenuScreen extends Entity implements Lifecycle {
 	}
 
 	@Override
-	public void init() {
-		fsm.init();
-	}
-
-	@Override
 	public void update() {
-		if (keyPressedOnce(VK_ENTER)) {
-			screenManager.selectPlayScreen(fsm.getState());
+		if (Keyboard.keyPressedOnce(KeyEvent.VK_ENTER)) {
+			screenManager.selectPlayScreen(getState());
 		}
-		fsm.update();
+		super.update();
 	}
 
 	@Override
@@ -121,17 +111,17 @@ public class MenuScreen extends Entity implements Lifecycle {
 		g.fillRect(0, 0, size.width, size.height);
 		g.setFont(new Font("Arial Black", Font.PLAIN, 28));
 		PlayMode[] playModes = PlayMode.values();
-		int y = 60;
+		int y = 70;
 		int h = size.height / playModes.length;
 		for (int i = 0; i < playModes.length; ++i) {
-			if (playModes[i] == fsm.getState()) {
+			if (playModes[i] == getState()) {
 				g.setColor(bgColorSelected);
 				g.fillRect(0, h * i, size.width, h);
 				g.setColor(hilightColor);
 			} else {
 				g.setColor(Color.WHITE);
 			}
-			String text = itemText(playModes[i]);
+			String text = playModeText(playModes[i]);
 			int w = g.getFontMetrics().stringWidth(text);
 			g.drawString(text, size.width / 2 - w / 2, y);
 			y += h;
@@ -139,7 +129,7 @@ public class MenuScreen extends Entity implements Lifecycle {
 		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
 	}
 
-	private String itemText(PlayMode mode) {
+	private String playModeText(PlayMode mode) {
 		switch (mode) {
 		case Computer_Computer:
 			return "Computer - Computer";
